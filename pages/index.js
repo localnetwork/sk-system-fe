05dc5ad9a5bc10933e3bf502a4aa3f0890374681
localnetwork <diomenikep@gmail.com>
2024-12-01 19:35:24 +0800

    chore: update login style

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,29 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      <form action="#">
-        <div>
-          <label htmlFor="username" className="tesgt">
-            Username:
-          </label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input type="text" />
-        </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+    <div className="py-[100px]">
+      <div className="container mx-auto">
+        <form action="#" className="flex flex-col gap-[15px]">
+          <div className="flex gap-[5px]">
+            <label htmlFor="username" className="tesgt asdasasd">
+              Username:
+            </label>
+            <input type="text" className="shadow-md border" />
+          </div>
+          <div className="flex gap-[5px]">
+            <label htmlFor="username">Password:</label>
+            <input type="text" className="shadow-md border" />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="bg-[red] text-white px-[15px] py-[10px] rounded-[10px] min-w-[150px]"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
